422e43e8e25a9775859df4dfb0c7d5a0c83725cd
rakshitar1135 <rakshitaraju2000@gmail.com>
2022-12-23 20:44:02 +0530

    Increase bY 5 and 10

--- src/Store/index.js
+++ src/Store/index.js
@@ -5,11 +5,17 @@ const counterReducer=(state={counter:0},action)=>{
     counter:state.counter+1
     }
 }
+if(action.type==='increase'){
+    return{
+    counter:state.counter+action.amount,
+    }
+}
 if(action.type==='decrement'){
     return{
     counter:state.counter-1
     }
 }
+
 return state;
 };
 const store=createStore(counterReducer);
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,12 +10,16 @@ const Counter = () => {
   const decrementHandler=()=>{
    dispatch({type:'decrement'})
   }
+  const increasehandler=()=>{
+   dispatch({type:'increase',amount:10})
+  }
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={increasehandler}>Increase By 10</button>
       </div>
       <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
